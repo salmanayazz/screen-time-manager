50dda43b69e342039ca8bbd48bf0b5d467a5a7ec
Salman Ayaz <salmanayazz@gmail.com>
2023-11-24 19:18:41 -0800

    integrated local db with repos & modified getUsageData params

--- app/src/main/java/com/example/screentimemanager/data/repository/UsageRepository.kt
+++ app/src/main/java/com/example/screentimemanager/data/repository/UsageRepository.kt
@@ -2,19 +2,48 @@ package com.example.screentimemanager.data.repository
 
 import com.example.screentimemanager.data.firebase.usage.UsageFirebase
 import com.example.screentimemanager.data.firebase.usage.UsageFirebaseDao
+import com.example.screentimemanager.data.local.usage.Usage
+import com.example.screentimemanager.data.local.usage.UsageDao
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UsageRepository(
-    private val usageDao: UsageFirebaseDao
+    private val usageFirebaseDao: UsageFirebaseDao,
+    private val usageDao: UsageDao
 ) {
+
     /**
-     * @param email
-     * email of the user whose usage data is being retrieved
+     * returns for usage data for the given user on the given date
+     * @param day
+     * the day of the month
+     * @param month
+     * the month of the year
+     * @param year
+     * the year
      * @return
      * return list of the user's usage data
      */
-    suspend fun getUsageData(email: String): List<UsageFirebase> {
-        return usageDao.getUsageData(email)
+    suspend fun getUsageData(day: Int, month: Int, year: Int) : List<Usage> {
+        return usageDao.getUsageData(day, month, year)
+    }
+
+    /**
+     * returns for usage data for the given user on the given date
+     * @param email
+     * email of the user whose usage data is being retrieved
+     * @param day
+     * the day of the month
+     * @param month
+     * the month of the year
+     * @param year
+     * the year
+     * @return
+     * return list of the user's usage data
+     */
+    suspend fun getUsageData(email: String, day: Int, month: Int, year: Int): List<UsageFirebase> {
+        return usageFirebaseDao.getUsageData(email, day, month, year)
     }
 
     /**
@@ -22,6 +51,8 @@ class UsageRepository(
      * the app to add to the user's list of apps
      */
     suspend fun setUsageData(appName: String, date: String, usage: Long) {
-        return usageDao.setUsageData(appName, date, usage)
+        CoroutineScope(Dispatchers.IO).launch {
+            usageFirebaseDao.setUsageData(appName, date, usage)
+        }
     }
 }
