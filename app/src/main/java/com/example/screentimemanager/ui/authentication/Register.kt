16355a43fa6bd13336081530d0a3876b2f963a11
M-Parsaei <khodadostetdarm@gmail.com>
2023-11-26 20:03:21 -0700

    chore: added the login and register activities

---
+++ app/src/main/java/com/example/screentimemanager/ui/authentication/Register.kt
@@ -0,0 +1,12 @@
+package com.example.screentimemanager.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.screentimemanager.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
