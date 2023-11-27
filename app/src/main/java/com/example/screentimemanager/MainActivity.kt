2fda50129b837295dddbaafc7a27b7b8555eebce
M-Parsaei <khodadostetdarm@gmail.com>
2023-11-27 02:50:57 -0700

    fixed and added listener for items on swipe menu

--- app/src/main/java/com/example/screentimemanager/MainActivity.kt
+++ app/src/main/java/com/example/screentimemanager/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.screentimemanager
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +15,7 @@ import com.example.screentimemanager.appusage.AppUsageService
 import com.example.screentimemanager.data.firebase.user.UserFirebase
 import com.example.screentimemanager.data.firebase.user.UserFirebaseDao
 import com.example.screentimemanager.databinding.ActivityMainBinding
+import com.example.screentimemanager.ui.authentication.Login
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -32,6 +35,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navigationView: NavigationView
     private lateinit var menuName: TextView
     private lateinit var menuEmail: TextView
+    private lateinit var signOutBtn: Button
+    private lateinit var exitBtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -58,31 +63,36 @@ class MainActivity : AppCompatActivity() {
 
         // setting up the swipe bar menu
         navigationView = findViewById(R.id.navigation_view)
-        var headerView = navigationView.getHeaderView(0)
-        menuName = headerView.findViewById(R.id.nav_header_user_name)
-        menuEmail = headerView.findViewById(R.id.nav_header_email)
+        menuName = findViewById(R.id.nav_header_user_name)
+        menuEmail = findViewById(R.id.nav_header_email)
+        signOutBtn = findViewById(R.id.signout_btn_menu)
+        exitBtn = findViewById(R.id.exit_btn_menu)
+
+
+        signOutBtn.setOnClickListener{
+            onSignOutListener()
+        }
+        exitBtn.setOnClickListener{
+            finishAffinity()
+        }
 
         // update the user information on swipe menu
         updateUserSwipeMenu()
 
 
-        // setting the sign out click listener
-        navigationView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.menu_sign_out -> {
-                    firebaseAuth.signOut()
-                    updateUserSwipeMenu()
-                    true
-                }
-                else -> false
-            }
-        }
-
-
-
         // start the AppUsageService
         val serviceIntent = Intent(this, AppUsageService::class.java)
         this.startForegroundService(serviceIntent)
+    }
+
+
+    private fun onSignOutListener(){
+        firebaseAuth.signOut()
+        updateUserSwipeMenu()
+    }
+
+    private fun onSignInListener(){
+        startActivity(Intent(this, Login::class.java))
     }
 
     private fun updateUserSwipeMenu(){
@@ -101,6 +111,10 @@ class MainActivity : AppCompatActivity() {
         }else{
             menuName.text = "User"
             menuEmail.text= "user@exmaple.com"
+            signOutBtn.text = "Sign In"
+            signOutBtn.setOnClickListener{
+                onSignInListener()
+            }
         }
     }
 }
