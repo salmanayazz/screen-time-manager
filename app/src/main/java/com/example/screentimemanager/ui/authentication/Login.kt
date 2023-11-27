f183579a461cbba3650c609a306d70c97527bf86
M-Parsaei <khodadostetdarm@gmail.com>
2023-11-26 21:01:20 -0700

    chore: finished the register activity layout

--- app/src/main/java/com/example/screentimemanager/ui/authentication/Login.kt
+++ app/src/main/java/com/example/screentimemanager/ui/authentication/Login.kt
@@ -1,12 +1,20 @@
 package com.example.screentimemanager.ui.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.screentimemanager.R
 
 class Login : AppCompatActivity() {
+    private lateinit var registerTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        registerTextView = findViewById<TextView>(R.id.register_button_text)
+        registerTextView.setOnClickListener{
+            var intent:Intent= Intent(this,Register::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/screentimemanager/ui/authentication/Register.kt
+++ app/src/main/java/com/example/screentimemanager/ui/authentication/Register.kt
@@ -1,12 +1,20 @@
 package com.example.screentimemanager.ui.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.screentimemanager.R
 
 class Register : AppCompatActivity() {
+    private lateinit var loginTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        loginTextView = findViewById<TextView>(R.id.login_button_text)
+        loginTextView.setOnClickListener{
+            finish() // to go back to login activity
+        }
     }
 }
