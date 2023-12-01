0e7ae5838b6b93dadbf6d10d75ef31804148e623
M-Parsaei <khodadostetdarm@gmail.com>
2023-11-30 18:46:54 -0700

    chore: added initial profile setting activity

---
+++ app/src/main/java/com/example/screentimemanager/ui/profileSetting/ProfileSetting.kt
@@ -0,0 +1,12 @@
+package com.example.screentimemanager.ui.profileSetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.screentimemanager.R
+
+class ProfileSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_setting)
+    }
+}
